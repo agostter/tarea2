eb7ab22880ed354b379a25f59424150257f26635
agostter <termineagostina@gmail.com>
2022-11-17 12:50:41 -0300

    javascript

---
+++ js/02.js
@@ -0,0 +1,12 @@
+
+console.log ("Trabajamos con JavaScript y Node.js")
+
+//Condicionales
+
+if (true) {
+    console.log("Es true y ejecuto los códigos de las llaves");
+}
+if (false) {
+    console.log("Es false y no ejecuto los códigos de las llaves");
+
+}
---
+++ js/index.js
@@ -0,0 +1,53 @@
+console.log ("Buenos días. Soy el código JS del extremo del html")
+
+console.log ('Buenos días. Soy el código JS del extremo del html')
+
+//Variables
+//Palabras reservadas
+var nombre;
+
+nombre="Agostina"
+
+console.log ("Mi nombre es: " + nombre);
+
+let apellido ="Termine"
+console.log ("Mi apellido es: " + apellido);
+
+const provincia = "Salta";
+const DNI = 123456789;
+const PI= 3.14
+
+nombre = "Pepe";
+apellido = "Sanchez";
+
+// datos de entrada
+
+let persona= prompt (
+    "Ingresa tu nombre: "
+)
+alert("Bienvenido " + persona);
+
+
+let numero = parseInt (prompt ("Ingresa un numero: "));
+let numeroDos = parseInt (prompt ("Ingresa un numero: "));
+
+let resultado = numero + numeroDos;
+alert("El resultado de la suma es: ${resultado}")
+
+let a ="100";
+let b = "150";
+console.log= (a + b);
+
+let usuario = promt ("Ingresa tu usuario: ");
+let pass = parseInt (prompt("Ingresa tu password numerico: "));
+
+if( usuario == "admin" && pass == 1234){
+    alert("Bienvenido Administrador")
+} else {
+    alert("usuario o password incorrecto")
+}
+
+
+
+
+
